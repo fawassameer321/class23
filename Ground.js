800081d933c82f05814120df3b5765465f9de2c3
fawassameer321 <77887545+fawassameer321@users.noreply.github.com>
2021-02-13 17:33:17 +0300

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,20 @@
+class Ground {
+    constructor() {
+      var options = {
+         isStatic : true
+      }
+      this.body = Bodies.rectangle(200,390,400,20,options);
+      this.width = 400;
+      this.height = 20
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("yellow");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
+  
